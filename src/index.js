98dbb06deb6daf58d1666b0c302f906983e97075
ParkYoDo <scarletdemon@naver.com>
2023-03-24 04:14:30 +0900

    favicon 변경

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import TodoStore from './components/TodoStore';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 
+const persistor = persistStore(TodoStore);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let persistor = persistStore(TodoStore);
+
 root.render(
   <React.StrictMode>
     <Provider store={TodoStore}>
@@ -22,8 +21,3 @@ root.render(
     </Provider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
